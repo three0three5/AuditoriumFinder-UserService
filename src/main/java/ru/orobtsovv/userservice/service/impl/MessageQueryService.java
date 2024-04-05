8659260e1a2eef640bc77dc8723dee1f9bfdcd92
stas <orobtsovv@gmail.com>
2024-04-05 16:50:33 +0300

    cleanup

--- src/main/java/ru/orobtsovv/userservice/service/impl/MessageQueryService.java
+++ src/main/java/ru/orobtsovv/userservice/service/impl/MessageQueryService.java
@@ -1,12 +1,14 @@
 package ru.orobtsovv.userservice.service.impl;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 import ru.orobtsovv.userservice.dto.messages.ProfileDeleteMessage;
 import ru.orobtsovv.userservice.dto.request.BanRequest;
 
 @Service
 @RequiredArgsConstructor
+@Profile("with-mocks")
 public class MessageQueryService {
 
     public void sendDeletedProfile(BanRequest request) {
--- src/main/java/ru/orobtsovv/userservice/service/impl/NotificationService.java
+++ src/main/java/ru/orobtsovv/userservice/service/impl/NotificationService.java
@@ -1,15 +1,17 @@
 package ru.orobtsovv.userservice.service.impl;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 import ru.orobtsovv.userservice.dto.response.ShortMessageResponse;
 
 @Service
 @RequiredArgsConstructor
+@Profile("with-mocks")
 public class NotificationService {
 
     public ShortMessageResponse subscribe(int fromId, int toId) {
-        return null;
+        return null; // TODO
     }
 
     public ShortMessageResponse unsubscribe(int fromId, int toId) {
