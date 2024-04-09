c408395e67bb042bdbec9f8e3c34b8984257c655
stas <orobtsovv@gmail.com>
2024-04-09 16:10:06 +0300

    cleanup

--- src/main/java/ru/orobtsovv/userservice/exception/RequestAlreadyException.java
+++ src/main/java/ru/orobtsovv/userservice/exception/RequestAlreadyException.java
@@ -1,6 +1,5 @@
 package ru.orobtsovv.userservice.exception;
 
-import static ru.orobtsovv.userservice.utils.constants.ExceptionConstants.FRIENDS_ALREADY;
 import static ru.orobtsovv.userservice.utils.constants.ExceptionConstants.REQUEST_EXISTS;
 
 public class RequestAlreadyException extends RuntimeException {
